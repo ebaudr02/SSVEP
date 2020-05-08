9a0b22f42acdc0c41f8bc0bb9ba6af10d77b3d94
ebaudr02 <ebaudr02@gmail.com>
2020-05-08 18:40:55 +0100

    Triangle correlation matrix, max and min correlation.

--- A001SM1Analysis.py
+++ A001SM1Analysis.py
@@ -11,54 +11,61 @@ import pandas as pd
 import re
 import matplotlib.pyplot as plt
 from getIndexes import getIndexes
+import seaborn as sn
+import numpy as np
 
-#OPENING AND CLEANING DATA
-df = pd.read_csv("/home/test/Documents/DataScience/A001SM1_1.csv")
+#OPEN AND CLEAN DATA
+df = pd.read_csv("A001SM1_1.csv")
 df.shape
 data = df.iloc[:,3:]
 
-#PLOTTING DATA
-#graph of each channel, mHz per time
-for (colName, colData) in data.iteritems():
-    fig = plt.figure()
-    plt.plot(data.index/128, colData.values, label=colName)
-    plt.title(colName)
-    plt.xlabel('Time (s)')
-    plt.ylabel('mHz')
-    plt.legend()
+# #PLOT DATA
+# #graph of each channel, mHz per time
+# for (colName, colData) in data.iteritems():
+#     fig = plt.figure()
+#     plt.plot(data.index/128, colData.values, label=colName)
+#     plt.title(colName)
+#     plt.xlabel('Time (s)')
+#     plt.ylabel('mHz')
+#     plt.legend()
     
-#graph of all channels to compare the time difference in response
-fig = plt.figure(figsize=(13,13))
-for (colName, colData) in data.iteritems():
-    if (bool(re.match('F',colName))):
-        plt.plot(data.index/128, colData.values, label=colName, linestyle='dashed')
-    else:
-        plt.plot(data.index/128, colData.values, label=colName)
-plt.title("All channels")
-plt.xlabel('Time (s)')
-plt.ylabel('mHz')
-plt.legend()
+# #graph of all channels to compare the time difference in response
+# fig = plt.figure(figsize=(13,13))
+# for (colName, colData) in data.iteritems():
+#     if (bool(re.match('F',colName))):
+#         plt.plot(data.index/128, colData.values, label=colName, linestyle='dashed')
+#     else:
+#         plt.plot(data.index/128, colData.values, label=colName)
+# plt.title("All channels")
+# plt.xlabel('Time (s)')
+# plt.ylabel('mHz')
+# plt.legend()
 
 #EXTREME VALUES AND CENTRAL TENDENCY
 #channel where max response is seen
 listMax = data.max(0)
 dataMax = listMax.max()
-print('Channel for maximum value: ', list(listMax[listMax==dataMax].index))
+# print('Channel for maximum value: ', list(listMax[listMax==dataMax]This.index))
+
 
 #time the max happens for each channel
 channelTimeOfMax = getIndexes(data,listMax)
 channelTimeOfMax.sort()
 
-#Channels mean voltage distribution
-fig = plt.figure()
-plt.hist(data.mean(0))
-plt.title('Channels mean voltage distribution')
-plt.xlabel('mHz')
+# #Channels mean voltage distribution
+# fig = plt.figure()
+# plt.hist(data.mean(0))
+# plt.title('Channels mean voltage distribution')
+# plt.xlabel('mHz')
 
 
 #STATISTICAL ANALYSIS
-#variance of each channel
+correlMatrix = data.corr().where(np.tril(np.ones(data.corr().shape),-1).astype(np.bool))
+sn.heatmap(correlMatrix)
+plt.show()
+print('Correlation matrix triled: \n', correlMatrix)
 
-#correlation between paired-electrodes (O1-O2, P7-P8 ...)
-
-#correlation between brain zones (O1-P7, T7-F7 ...)
+correlMatrixMax =pd.DataFrame({'Max' : correlMatrix.max(0), 'Index of max' : correlMatrix.idxmax(0)})
+correlMatrixMin =pd.DataFrame({'Min' : correlMatrix.min(0), 'Index of min' : correlMatrix.idxmin(0)})
+print('Correlation matrix min: \n', correlMatrixMin)
+print('Correlation matrix max: \n', correlMatrixMax)
